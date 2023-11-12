b1dabb24021ad6267877735b982137b6ba4932c8
lnvictor <lnvictor@github.com>
2023-11-12 16:01:05 -0300

    Providing useful classes for algorithms

---
+++ dummy-compressor/Utils/LZ77Metadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dummy_compressor.Utils
+{
+    public class LZ77Metadata
+    {
+        public char NextChar { get; set; }
+        public int Offset { get; set; }
+        public int Length { get; set; }
+
+        public LZ77Metadata(char nextChar, int offset, int length) 
+        {
+            NextChar = nextChar;
+            Offset = offset;
+            Length = length;
+        }
+    }
+}
