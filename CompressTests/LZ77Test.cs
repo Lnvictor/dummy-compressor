c16fbb38e3c5f19ac8c51f797d6837572c88fdbf
victor.barbosa <victor.barbosa@kantaribopemedia>
2023-11-14 13:45:41 -0300

    implementing tests with files

--- CompressTests/LZ77Test.cs
+++ CompressTests/LZ77Test.cs
@@ -17,13 +17,12 @@ namespace CompressTests
         public void ShouldFindLongestMatch()
         {
             var searchBuffer = new List<char>("abcadsada".ToCharArray());
-            var lookaheadBuffer = new List<char>("bcopkpioj".ToCharArray());
+            var lookaheadBuffer = new List<char>("bcadsaioj".ToCharArray());
 
-            LZ77Metadata output = Compressor.findMatch(searchBuffer, lookaheadBuffer);
+            LZ77Metadata output = Compressor.FindMatch(searchBuffer, lookaheadBuffer);
             Assert.IsNotNull(output);
-            Assert.That(output.Length, Is.EqualTo(2));
-            Assert.That(output.Offset, Is.EqualTo(7));
-            Assert.That(output.NextChar, Is.EqualTo('o'));
+            Assert.That(output.Length, Is.EqualTo(6));
+            Assert.That(output.Offset, Is.EqualTo(8));
         }
 
         [Test]
@@ -32,11 +31,47 @@ namespace CompressTests
             var searchBuffer = new List<char>("abcadsada".ToCharArray());
             var lookaheadBuffer = new List<char>("lkopkpioj".ToCharArray());
 
-            LZ77Metadata output = Compressor.findMatch(searchBuffer, lookaheadBuffer);
+            LZ77Metadata output = Compressor.FindMatch(searchBuffer, lookaheadBuffer);
             Assert.IsNotNull(output);
             Assert.That(output.Length, Is.EqualTo(0));
             Assert.That(output.Offset, Is.EqualTo(0));
-            Assert.That(output.NextChar, Is.EqualTo('l'));
+        }
+
+        [Test]
+        public void ShouldCompressTooLongText()
+        {
+            string input = "refCgSqexBhBSLtQ7u3G5XeU30DPkv7C1p64xMdHdunEzrZRXpwNuRzxu3J9BhBSLtQ74cWr1ZpWAQg1R8ihie5V4yr3GSXQ13Hb";
+            string output = Compressor.Compress(input);
+            string expectedOutput = "refCgSqexBhBSLtQ7u3G5XeU30DPkv7C1p64xMdHdunEzrZRXpwNuRzxu3J9(51,8)4cWr1ZpWAQg1R8ihie5V4yr3GSXQ13Hb";
+
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void TestUsingFile()
+        {
+            string fileContent;
+
+            using (StreamReader reader = new StreamReader("C:\\Users\\victor.barbosa\\Documents\\personal\\dummy-compressor\\dummy-compressor\\TestFiles\\TestEncoding.txt"))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            string compressed = Compressor.Compress(fileContent);
+
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\victor.barbosa\\Documents\\personal\\dummy-compressor\\dummy-compressor\\TestFiles\\output.txt"))
+            {
+                writer.Write(compressed);
+            }
+
+            string decompressed = Compressor.Decompress(compressed);
+
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\victor.barbosa\\Documents\\personal\\dummy-compressor\\dummy-compressor\\TestFiles\\output_2.txt"))
+            {
+                writer.Write(decompressed);
+            }
+
+            Assert.That(decompressed.Length, Is.GreaterThanOrEqualTo(compressed.Length));
         }
     }
 }
