4379501cb78d6ea563fc3dbadacf533d640cebb8
victor.barbosa <victor.barbosa@kantaribopemedia>
2023-11-14 13:43:03 -0300

    Refactoring LZ77 Metadata to maybe contain the actual string

--- dummy-compressor/Utils/HuffmanTree.cs
+++ dummy-compressor/Utils/HuffmanTree.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace dummy_compressor.Utils
+﻿namespace dummy_compressor.Utils
 {
     public class HuffmanTree
     {
--- dummy-compressor/Utils/LZ77Metadata.cs
+++ dummy-compressor/Utils/LZ77Metadata.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace dummy_compressor.Utils
+﻿namespace dummy_compressor.Utils
 {
     public class LZ77Metadata
     {
-        public char NextChar { get; set; }
         public int Offset { get; set; }
         public int Length { get; set; }
 
-        public LZ77Metadata(char nextChar, int offset, int length) 
+        public string? Value { get; set; }
+
+        public LZ77Metadata(string? value, int offset, int length)
         {
-            NextChar = nextChar;
+            Value = value;
             Offset = offset;
             Length = length;
         }
